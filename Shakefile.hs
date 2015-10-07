af3b932b57d8b94e9c3fcea57a85b47280fdbddc
TheArtist <agorglouk@gmail.com>
2015-10-07 13:43:58 +0300

    Added dummy command line arguments for variant, arch and toolchain

--- Shakefile.hs
+++ Shakefile.hs
@@ -1,10 +1,12 @@
 module Shakefile where
 
-import Data.IORef
+import Data.Maybe
 import Development.Shake
 import Development.Shake.FilePath
 import Development.Shake.Util
 import System.Console.ANSI
+import System.Console.GetOpt
+import Text.Read
 import qualified Data.ByteString.Char8 as BS
 import qualified System.Info as System
 
@@ -53,6 +55,12 @@ data LinkResult =
   | SharedLibrary   -- ^ Shared (dynamically linked) library
   deriving (Enum, Eq, Show)
 
+-- | Build variant type
+data BuildVariant =
+    Release         -- ^ Release builds
+  | Debug           -- ^ Debug builds
+  deriving (Enum, Eq, Show, Read)
+
 ---------------------------------------------------------------------------
 -- | Toolchain
 ---------------------------------------------------------------------------
@@ -61,7 +69,7 @@ data ToolChainVariant =
     GCC       -- ^ GNU Compiler Collection (gcc) toolchain
   | LLVM      -- ^ Low-Level Virtual Machine (LLVM) toolchain
   | MSVC      -- ^ Microsoft Visual C++ (msvc) toolchain
-  deriving (Eq, Show)
+  deriving (Eq, Show, Read)
 
 ---------------------------------------------------------------------------
 -- | Toolchain Detail
@@ -155,6 +163,12 @@ os =
     "linux"   -> Linux
     _         -> error $ "Unknown host operating system: " ++ System.os
 
+-- | CPU Arch type
+data Arch =
+    I386
+  | X86_64
+  deriving (Eq, Ord, Show, Read)
+
 ---------------------------------------------------------------------------
 -- FileUtils
 ---------------------------------------------------------------------------
@@ -211,17 +225,71 @@ languageOf = flip lookup defaultLanguageMap . takeExtension
 -}
 
 ---------------------------------------------------------------------------
+-- | Build Parameters
+---------------------------------------------------------------------------
+-- The datatype that holds any flag information
+data AdditionalFlag = BVFlag BuildVariant | AFlag Arch | TVFlag ToolChainVariant deriving Eq
+
+-- Used by ReqArg to parse Build Variant Flag
+parseBuildVariantFlag :: String -> Either String AdditionalFlag
+parseBuildVariantFlag s =
+    case readMaybe s :: Maybe BuildVariant of
+        Nothing -> Left "Could not parse given build variant."
+        Just x  -> Right (BVFlag x)
+
+-- Used by ReqArg to parse Arch Flag
+parseArchFlag :: String -> Either String AdditionalFlag
+parseArchFlag s =
+    case readMaybe s :: Maybe Arch of
+        Nothing -> Left "Could not parse given architecture."
+        Just x  -> Right (AFlag x)
+
+-- Used by ReqArg to parse ToolChain Variant Flag
+parseTVFlag :: String -> Either String AdditionalFlag
+parseTVFlag s =
+    case readMaybe s :: Maybe ToolChainVariant of
+        Nothing -> Left "Could not parse given toolchain variant."
+        Just x  -> Right (TVFlag x)
+
+additionalFlags :: [OptDescr (Either String AdditionalFlag)]
+additionalFlags =
+    [ Option [] ["variant"]   (ReqArg parseBuildVariantFlag "VARIANT") "The build variant to make (Release/Debug)"
+    , Option [] ["arch"]      (ReqArg parseArchFlag "ARCH")            "The target architecture to build for (i386/x86_64)"
+    , Option [] ["toolchain"] (ReqArg parseTVFlag "TOOLCHAIN")         "The toolchain to use for building the target"
+    ]
+
+---------------------------------------------------------------------------
 -- | Entrypoint
 ---------------------------------------------------------------------------
 main :: IO ()
 main = do
-    pRef <- newIORef $ return mempty
+    let opts = shakeOptions { shakeFiles = "tmp/.shake"
+                            , shakeOutput = const $ BS.putStr . BS.pack
+                            }
+    shakeArgsWith opts additionalFlags $ \flags targets -> return $ Just $ do
+        if null targets then want [binDir </> projName <.> exe] else want targets
 
-    shakeArgs shakeOptions { shakeFiles = "tmp/.shake"
-                           , shakeOutput = const $ BS.putStr . BS.pack
-                           , shakeProgress = writeIORef pRef
-                           } $ do
-        want [binDir </> projName <.> exe]
+        "banner" ~> do
+            -- Variant
+            let defVariant = Release
+            let variant = listToMaybe [v | BVFlag v <- flags]
+            case variant of
+                Nothing -> putNormal $ "No build variant given. Defaulting to " ++ show defVariant ++ "\n"
+                Just x  -> putNormal $ "Build variant: " ++ show x ++ "\n"
+
+            -- Arch
+            let defArch = I386
+            let arch = listToMaybe [v | AFlag v <- flags]
+            case arch of
+                Nothing -> putNormal $ "No target arch given. Defaulting to " ++ show defArch ++ "\n"
+                Just x  -> putNormal $ "Target arch: " ++ show x ++ "\n"
+
+            -- Toolchain
+            let defToolchain = GCC
+            let toolchain = listToMaybe [v | TVFlag v <- flags]
+            case toolchain of
+                Nothing -> putNormal $ "No toolchain variant given. Defaulting to " ++ show defToolchain ++ "\n"
+                Just x  -> putNormal $ "Using toolchain: " ++ show x ++ "\n"
 
         "clean" ~> do
             putNormal "Cleaning..."
@@ -230,9 +298,7 @@ main = do
 
         binDir </> projName <.> exe %> \out -> do
             -- Initial banner
-            let variant = "ReleaseDummy"
-            let arch = "x86 Dummy"
-            putNormal $ "Building " ++ variant ++ " variant for " ++ arch ++ " architecture" ++ "\n"
+            need ["banner"]
 
             -- Gather the source files
             srcfiles <- getDirectoryFiles "" ["src//*.cpp"]

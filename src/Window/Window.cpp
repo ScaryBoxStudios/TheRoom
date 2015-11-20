9bcca62feed1666bd34129f1b8e9ad4b86a42571
TheArtist <agorglouk@gmail.com>
2015-11-20 20:39:09 +0200

    Fixed warning about local variable name hidding function parameter

--- src/Window/Window.cpp
+++ src/Window/Window.cpp
@@ -41,11 +41,11 @@ bool Window::Create(int width, int height, const std::string& title, Mode mode)
         {
             mon = glfwGetPrimaryMonitor();
 
-            const GLFWvidmode* mode = glfwGetVideoMode(mon);
-            glfwWindowHint(GLFW_RED_BITS, mode->redBits);
-            glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
-            glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
-            glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+            const GLFWvidmode* videoMode = glfwGetVideoMode(mon);
+            glfwWindowHint(GLFW_RED_BITS, videoMode->redBits);
+            glfwWindowHint(GLFW_GREEN_BITS, videoMode->greenBits);
+            glfwWindowHint(GLFW_BLUE_BITS, videoMode->blueBits);
+            glfwWindowHint(GLFW_REFRESH_RATE, videoMode->refreshRate);
 
             break;
         }

b01341e8dd15cdb7d9e99db9a3106cadca5a6014
TheArtist <agorglouk@gmail.com>
2015-11-24 12:11:32 +0200

    Cube vertex list is now composed of vec3 elements instead of vec4

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -17,7 +17,7 @@ const GLchar* vertexShader = R"rsd(
 #version 330
 
 in vec2 in_texCoords;
-layout(location=0) in vec4 in_Pos;
+layout(location=0) in vec3 in_Pos;
 layout(location=1) in vec3 in_Col;
 out vec3 vertexColor;
 out vec2 out_texCoords;
@@ -27,7 +27,7 @@ uniform mat4 MVP;
 void main(void)
 {
     out_texCoords = in_texCoords;
-    gl_Position = MVP * in_Pos;
+    gl_Position = MVP * vec4(in_Pos, 1.0f);
     vertexColor = in_Col;
 }
 
@@ -191,7 +191,7 @@ void Game::GLInit()
     // Create VBOs
     glBindBuffer(GL_ARRAY_BUFFER, mGLData.vBuf);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertexes), cubeVertexes, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, (void*) 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*) 0);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 

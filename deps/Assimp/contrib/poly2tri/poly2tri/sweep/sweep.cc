16fc02d2660e955d11dddad4bb2e192644e90ea1
TheArtist <agorglouk@gmail.com>
2015-12-16 03:28:05 +0200

    Added missing define flag that caused Assimp build on MinGw to fail

--- deps/Assimp/contrib/poly2tri/poly2tri/sweep/sweep.cc
+++ deps/Assimp/contrib/poly2tri/poly2tri/sweep/sweep.cc
@@ -28,6 +28,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#define _USE_MATH_DEFINES
 #include <stdexcept>
 #include "sweep.h"
 #include "sweep_context.h"

f5a399a17ef9ce371b533252e57e60d85c8eb3ac
TheArtist <agorglouk@gmail.com>
2015-12-24 01:43:45 +0200

    Optimized RenderQuad call

--- src/Graphics/Renderer/Renderer.cpp
+++ src/Graphics/Renderer/Renderer.cpp
@@ -188,51 +188,32 @@ void Renderer::LightPass(float interpolation)
 
 void Renderer::RenderQuad()
 {
-    GLfloat quadPos[] =
+    GLfloat quadVert[] =
     {
-       -1.0f,  1.0f, 0.0f,
-       -1.0f, -1.0f, 0.0f,
-        1.0f,  1.0f, 0.0f,
-        1.0f, -1.0f, 0.0f,
-    };
-    GLfloat quadUV[] =
-    {
-        0.0f, 1.0f,
-        0.0f, 0.0f,
-        1.0f, 1.0f,
-        1.0f, 0.0f,
+       -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+       -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+        1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, 0.0f, 1.0f, 0.0f
     };
 
     GLuint quadVao;
     glGenVertexArrays(1, &quadVao);
     glBindVertexArray(quadVao);
     {
-        GLuint quadPosVbo;
-        glGenBuffers(1, &quadPosVbo);
-        glBindBuffer(GL_ARRAY_BUFFER, quadPosVbo);
-        {
-            glBufferData(GL_ARRAY_BUFFER, sizeof(quadPos), &quadPos, GL_STATIC_DRAW);
-            glEnableVertexAttribArray(0);
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), nullptr);
-        }
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        GLuint quadVbo;
+        glGenBuffers(1, &quadVbo);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVbo);
 
-        GLuint quadUvVbo;
-        glGenBuffers(1, &quadUvVbo);
-        glBindBuffer(GL_ARRAY_BUFFER, quadUvVbo);
-        {
-            glBufferData(GL_ARRAY_BUFFER, sizeof(quadUV), &quadUV, GL_STATIC_DRAW);
-            glEnableVertexAttribArray(2);
-            glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), nullptr);
-        }
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVert), &quadVert, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), nullptr);
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 
-        glBindBuffer(GL_ARRAY_BUFFER, quadPosVbo);
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-        glDeleteBuffers(1, &quadUvVbo);
-        glDeleteBuffers(1, &quadPosVbo);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glDeleteBuffers(1, &quadVbo);
     }
     glBindVertexArray(0);
     glDeleteVertexArrays(1, &quadVao);

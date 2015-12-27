f39b4352f77401897d9efb536271211861354bdf
TheArtist <agorglouk@gmail.com>
2015-12-27 02:52:40 +0200

    Moved sfinae trick from template declaration to using directive

--- src/Util/Hash.hpp
+++ src/Util/Hash.hpp
@@ -37,18 +37,22 @@
 namespace std
 {
     template<typename T>
-    struct hash
+    struct enum_class_hash
     {
+        using sfinae = typename std::enable_if<std::is_enum<T>::value>::type;
+
         using argument_type = T;
         using underlying_type = typename std::underlying_type<argument_type>::type;
         using result_type = typename std::hash<underlying_type>::result_type;
-        template <typename U = T, typename std::enable_if<std::is_enum<U>::value, int>::type = 0>
         result_type operator()(const argument_type& arg) const
         {
             std::hash<underlying_type> hasher;
             return hasher(static_cast<underlying_type>(arg));
         }
     };
+
+    template <typename T>
+    struct hash : public enum_class_hash<T> {};
 }
 
 #endif // ! _HASH_FIX_HPP_

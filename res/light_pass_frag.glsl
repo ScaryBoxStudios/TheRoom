6b81a41a344ccdc14f49377f4a6df2a7d4de72ba
TheArtist <agorglouk@gmail.com>
2016-01-10 02:27:48 +0200

    Added simple PCF algorithm to soften the shadows

--- res/light_pass_frag.glsl
+++ res/light_pass_frag.glsl
@@ -138,7 +138,19 @@ float ShadowCalculation(vec4 fragPosLightSpace)
     float currentDepth = projCoords.z;
     // Check whether current frag pos is in shadow
     float bias = 0.005;
-    float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+
+    // PCF
+    float shadow = 0.0;
+    vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+    for(int x = -1; x <= 1; ++x)
+    {
+        for(int y = -1; y <= 1; ++y)
+        {
+            float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r;
+            shadow += currentDepth - bias > pcfDepth  ? 1.0 : 0.0;
+        }
+    }
+    shadow /= 9.0;
 
     return shadow;
 }

e4e0de3be2cfad3ca68174df103349b09b6b125f
TheArtist <agorglouk@gmail.com>
2016-01-21 12:08:50 +0200

    ImageLoader factory now supports Png images

--- src/Graphics/Image/ImageLoader.cpp
+++ src/Graphics/Image/ImageLoader.cpp
@@ -1,5 +1,7 @@
 #include "ImageLoader.hpp"
 #include <stdexcept>
+#include <istream>
+#include <streambuf>
 #include "../../Util/FileLoad.hpp"
 
 #include "../../Util/WarnGuard.hpp"
@@ -8,6 +10,17 @@ WARN_GUARD_ON
 #include "Jpeg/JpegLoader.hpp"
 #include "Tga/TgaLoader.hpp"
 WARN_GUARD_OFF
+
+// Helper to access a Buffer type using an istream
+template<typename CharT, typename TraitsT = std::char_traits<CharT> >
+class vectorwrapbuf : public std::basic_streambuf<CharT, TraitsT>
+{
+    public:
+        vectorwrapbuf(std::vector<CharT> &vec)
+        {
+            setg(vec.data(), vec.data(), vec.data() + vec.size());
+        }
+};
 
 auto ImageLoader::Load(const Buffer& buf, const std::string& hint) -> RawImage<Buffer>
 {
@@ -20,6 +33,29 @@ auto ImageLoader::Load(const Buffer& buf, const std::string& hint) -> RawImage<B
     {
         TgaLoader tgaLoader;
         return tgaLoader.Load(buf);
+    }
+    else if (hint == "png")
+    {
+        // Create png++ image object
+        using PngPixelType = png::rgba_pixel;
+        using PngBufferType = png::solid_pixel_buffer<PngPixelType>;
+        png::image<PngPixelType, PngBufferType> img;
+
+        // Pass a istream object that reads throught the buf to the read_stream func
+        vectorwrapbuf<char> wb((std::vector<char>&)(buf));
+        std::istream is(&wb);
+        img.read_stream(is);
+
+        // Create the RawImage object from the read png++ image
+        auto& pb = img.get_pixbuf();
+        return RawImage<>(
+            pb.fetch_bytes(),
+            {
+                static_cast<uint32_t>(pb.get_width()),
+                static_cast<uint32_t>(pb.get_height()),
+                static_cast<uint8_t>(PngBufferType::pixel_traits_t::get_channels())
+            }
+        );
     }
     return RawImage<>({}, {0,0,0});
 }

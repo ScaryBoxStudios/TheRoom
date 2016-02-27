c455084b27d50e55f4825c116ee0b3f6b40ef3bc
TheArtist <agorglouk@gmail.com>
2016-02-27 23:53:29 +0200

    Material id storing is now being done in the correct buffer texture

--- res/geometry_pass_frag.glsl
+++ res/geometry_pass_frag.glsl
@@ -3,7 +3,7 @@
 layout (location = 0) out vec3 gPosition;
 layout (location = 1) out vec3 gNormal;
 layout (location = 2) out vec4 gAlbedoSpec;
-layout (location = 4) out vec3 gMatIdx;
+layout (location = 3) out uint gMatIdx;
 
 in VS_OUT
 {

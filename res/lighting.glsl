470688c4d4aadd9275d360daf28989a3c2ae7512
TheCrafter <dimitrisvlh@gmail.com>
2016-04-12 21:01:37 +0300

    Fixed BRDF to make it physically correct

--- res/lighting.glsl
+++ res/lighting.glsl
@@ -47,6 +47,8 @@ struct SpotLight
     float outerCutOff;
 };
 
+float pi = 3.14159265;
+
 // Calculates light attenuation value, according to its attenuation properties, the light position, and the fragment position
 float CalcAttenuationValue(AttenuationProps attProps, vec3 lightPos, vec3 fragPos)
 {
@@ -83,75 +85,69 @@ float GeometricalAttenuation(float NdotH, float NdotV, float VdotH, float NdotL)
 
 float BeckmannDistribution(float roughness, float NdotH)
 {
-    float pi = 3.14159265;
     float a  = roughness * roughness;
     float NdotH2 = NdotH * NdotH;
     return exp((NdotH2 - 1.0) / (a * NdotH2)) / (pi * a * pow(NdotH, 4.0));
 }
 
-float Fresnel(float F0, float VdotH)
+// Slick's approximation
+// Pass the reflected color at normal angle directly
+vec3 Fresnel(vec3 R0, float VdotH)
 {
-    float F = pow(1.0 - VdotH, 5.0);
-    F *= (1.0 - F0);
-    F += F0;
-    return F;
-    //return mix(F0, 1, pow(1.0 - VdotH, 5.0));
+    float u  = 1.0 - VdotH;
+    float u5 = pow(u, 5);
+    return vec3(
+        min(1.0, R0.r + (1.0 - R0.r) * u5),
+        min(1.0, R0.g + (1.0 - R0.g) * u5),
+        min(1.0, R0.b + (1.0 - R0.b) * u5));
 }
 
-// Calculates specular intensity according to the Cook - Torrance model
-float CalcCookTorSpec(vec3 normal, vec3 lightDir, vec3 viewDir, float roughness, float F0)
+vec3 BRDF(vec3 N, vec3 L, vec3 V, vec3 baseColor, float metallic, float roughness, float reflectivity)
 {
-    // Calculate intermediary values
-    vec3 halfVector = normalize(lightDir + viewDir);
-    float NdotL = max(dot(normal, lightDir), 0.0);
-    float NdotH = max(dot(normal, halfVector), 0.0);
-    float NdotV = max(dot(normal, viewDir), 0.0); // Note: this could also be NdotL, which is the same value
-    float VdotH = max(dot(viewDir, halfVector), 0.0);
+    vec3  H     = normalize(L + V);
+    float NdotL = max(dot(N, L), 0.0);
+    float NdotH = max(dot(N, H), 0.0);
+    float NdotV = max(dot(N, V), 0.0);
+    float VdotH = max(dot(V, H), 0.0);
 
-    float specular = 0.0;
-    if(NdotL > 0.0)
-    {
-        float G = GeometricalAttenuation(NdotH, NdotV, VdotH, NdotL);
-        float D = BeckmannDistribution(roughness, NdotH);
-        float F = Fresnel(F0, VdotH);
+    // Specular vs Diffuse
+    vec3 C0 = baseColor * metallic + vec3(1.0) * (1.0 - metallic);
 
-        specular = (D * F * G) / (NdotV * NdotL * 4);
-    }
-    return specular;
+    // Calculate fresnel
+    vec3 F = Fresnel(C0, VdotH);
+
+    // Specular and diffuse contributions
+    vec3 Ks = F * reflectivity;
+    vec3 Kd = (1.0 - Ks) * (1.0 - metallic); // * (1.0 - transparency);
+
+    // Calculate Distribution and Geometrical Attenuation
+    float G = GeometricalAttenuation(NdotH, NdotV, VdotH, NdotL);
+    float D = BeckmannDistribution(roughness, NdotH);
+
+    // Final results
+    vec3 specular = Ks * D * G / (NdotV * NdotL * 4);
+    vec3 diffuse  = Kd * baseColor / pi;
+    return specular + diffuse;
 }
 
-float DiffuseLambert(vec3 normal, vec3 lightDir)
-{
-    return max(dot(normal, lightDir), 0.0);
-}
-
-// Internal func used by other Calc Light functions
 vec3 CalcLight(vec3 lightColor, vec3 normal, vec3 lightDir, vec3 viewDir, Material material, float shadowFactor)
 {
     // Helper variables
-    vec3  baseColor = material.diffuse;
-    vec3  specColor = material.specular;
-    vec3  emissive  = material.emissive;
-    float roughness = material.roughness;
-    float fresnel   = material.fresnel;
-    float metallic  = material.metallic;
+    vec3  baseColor    = material.diffuse;
+    vec3  emissive     = material.emissive;
+    float roughness    = material.roughness;
+    float reflectivity = material.fresnel;
+    float metallic     = material.metallic;
+    float NdotL        = max(dot(normal, lightDir), 0.0);
 
-    // Calculate contribution based on metallicity
-    vec3 diffuseColor  = baseColor - baseColor * metallic;
-    vec3 specularColor = mix(baseColor, vec3(0), roughness);
-    //vec3 specularColor = mix(vec3(0.00), baseColor, metallic);
+    // Calculate visibility
+    float visibility = 1.0 - shadowFactor;
 
-    // Lambertian reflectance
-    float Kd = DiffuseLambert(normal, lightDir);
-
-    // Specular shading (Cook-Torrance model)
-    float Ks = CalcCookTorSpec(normal, lightDir, viewDir, roughness, fresnel);
+    // BRDF
+    vec3 brdfResult = BRDF(normal, lightDir, viewDir, baseColor, metallic, roughness, reflectivity);
 
     // Combine results
-    vec3 diffuse  = diffuseColor * Kd;
-    vec3 specular = specularColor * Ks;
-    vec3 result   = lightColor * (emissive + diffuse + specular);
-    return result * (1.0 - shadowFactor);
+    return lightColor * NdotL * (brdfResult + emissive) * visibility;
 }
 
 // Calculates the color of directional light

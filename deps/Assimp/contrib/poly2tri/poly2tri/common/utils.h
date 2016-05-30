d3ee2133a1311f71a704ec163d14a0836552f052
TheArtist <agorglouk@gmail.com>
2016-05-30 04:49:45 +0300

    Fixed broken Assimp MinGW builds due to undefined Math constants

--- deps/Assimp/contrib/poly2tri/poly2tri/common/utils.h
+++ deps/Assimp/contrib/poly2tri/poly2tri/common/utils.h
@@ -38,6 +38,11 @@
 #include <exception>
 #include <math.h>
 
+#ifndef M_PI
+#define M_PI	3.14159265358979323846	/* pi */
+#define M_PI_2	1.57079632679489661923	/* pi/2 */
+#endif
+
 namespace p2t {
 
 const double PI_3div4 = 3 * M_PI / 4;

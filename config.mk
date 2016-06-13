84e2562bb63e74d30a82630520d30d431d1ea11a
TheArtist <agorglouk@gmail.com>
2016-06-13 16:38:10 +0300

    Added missing Linux link library

--- config.mk
+++ config.mk
@@ -6,5 +6,5 @@ ifeq ($(OS), Windows_NT)
 	LIBS += zlib rpcrt4 winmm opengl32 glu32 ole32 gdi32 advapi32 user32 shell32
 	DEFINES += _CRT_SECURE_NO_WARNINGS _CRT_SECURE_NO_DEPRECATE
 else
-	LIBS += z uuid GLU GL X11 Xcursor Xinerama Xrandr Xxf86vm Xi pthread
+	LIBS += z uuid GLU GL X11 Xcursor Xinerama Xrandr Xxf86vm Xi pthread dl
 endif

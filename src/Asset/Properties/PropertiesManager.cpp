186c89f486906ff09cb032348965902d72271e5c
TheCrafter <dimitrisvlh@gmail.com>
2016-09-30 08:35:10 +0300

    Reorganized PropertiesManager.cpp code

--- src/Asset/Properties/PropertiesManager.cpp
+++ src/Asset/Properties/PropertiesManager.cpp
@@ -1,6 +1,7 @@
 #include "PropertiesManager.hpp"
 
 #include <iostream>
+#include <memory>
 #include <unordered_map>
 
 #include "Properties.hpp"
@@ -10,39 +11,38 @@
 
 using BufferType = std::vector<std::uint8_t>;
 
+// --------------------------------------------------
+// Forward declarations for static functions
+// --------------------------------------------------
 // Util to load files
-auto LoadFile(const std::string& filename)
-{
-    auto rVal = FileLoad<BufferType>(filename);
-    if (!rVal)
-        throw std::runtime_error("Couldn't load file (" + filename + ")");
-    return rVal;
-}
+static std::unique_ptr<BufferType> LoadFile(const std::string& filename);
 
+// Util to load file data to containers
+static void LoadFiledata(
+    std::vector<const std::vector<PropertiesManager::LoadInput>*> input,
+    std::vector<PropertiesLoader::InputContainer*> out,
+    std::vector<std::unique_ptr<BufferType>>& dataBuf);
+
+// Print validation results
+static void PrintResult(const PropertiesValidator::Result& r);
+
+// --------------------------------------------------
+// Header function implementation
+// --------------------------------------------------
 void PropertiesManager::Load(
      const std::vector<PropertiesManager::LoadInput>& scenes,
      const std::vector<PropertiesManager::LoadInput>& materials,
      const std::vector<PropertiesManager::LoadInput>& models)
 {
-    // Create containers
-    std::vector<std::unique_ptr<BufferType>> filedata;
+    // Create input containers
     PropertiesLoader::InputContainer sceneIn, materialIn, modelIn;
+    std::vector<std::unique_ptr<BufferType>> filedata;
 
-    // Load files
-    {
-        std::vector<PropertiesLoader::InputContainer*> out = {&sceneIn, &materialIn, &modelIn};
-        size_t vecPos = 0;
-        for (const auto& i : {scenes, materials, models})
-        {
-            for (const auto& j : i)
-            {
-                filedata.push_back(LoadFile(j.filename));
-                out[vecPos]->insert({j.id, *filedata[filedata.size() - 1]});
-            }
-
-            ++vecPos;
-        }
-    }
+    // Load filedata
+    LoadFiledata(
+        {&scenes,  &materials,  &models},   // Input
+        {&sceneIn, &materialIn, &modelIn},  // Input containers
+        filedata);                          // Filedata buffer
 
     // Create the loader
     PropertiesLoader loader;
@@ -52,20 +52,60 @@ void PropertiesManager::Load(
     PropertiesLoader::OutputContainer<Properties::MaterialFile> materialOut;
     PropertiesLoader::OutputContainer<Properties::ModelFile> modelOut;
 
-    // Load
+    // Parse properties
     loader.LoadBulk(
         sceneIn,    sceneOut,
         materialIn, materialOut,
         modelIn,    modelOut);
 
-    // Validation
+    // Validate properties
     PropertiesValidator validator;
-        PropertiesValidator::Result r = validator.ValidateBulk(sceneOut, materialOut, modelOut);
+    PropertiesValidator::Result r = validator.ValidateBulk(sceneOut, materialOut, modelOut);
 
-    std::cout << "Errors: " << std::endl;
+    // Print results
+    PrintResult(r);
+}
+
+// --------------------------------------------------
+// Utilities
+// --------------------------------------------------
+static std::unique_ptr<BufferType> LoadFile(const std::string& filename)
+{
+    auto rVal = FileLoad<BufferType>(filename);
+    if (!rVal)
+        throw std::runtime_error("Couldn't load file (" + filename + ")");
+    return rVal;
+}
+
+static void LoadFiledata(
+    std::vector<const std::vector<PropertiesManager::LoadInput>*> input,
+    std::vector<PropertiesLoader::InputContainer*> out,
+    std::vector<std::unique_ptr<BufferType>>& dataBuf)
+{
+    size_t vecPos = 0;
+    for (const auto& i : input)
+    {
+        for (const auto& j : *i)
+        {
+            dataBuf.push_back(LoadFile(j.filename));
+            out[vecPos]->insert({j.id, *dataBuf[dataBuf.size() - 1]});
+        }
+
+        ++vecPos;
+    }
+}
+
+static void PrintResult(const PropertiesValidator::Result& r)
+{
+    // Errors
+    if (!r.errors.empty())
+        std::cout << "Errors: " << std::endl;
     for (const auto& e : r.errors)
         std::cout << "\t" << e.id + ": " << PropertiesValidator::ErrorToString(e.code) << std::endl;
-    std::cout << "Warnings: " << std::endl;
+
+    // Warnings
+    if (!r.warnings.empty())
+        std::cout << "Warnings: " << std::endl;
     for (const auto& w : r.warnings)
         std::cout << "\t" << w.id + ": " << PropertiesValidator::WarnToString(w.code) << std::endl;
 

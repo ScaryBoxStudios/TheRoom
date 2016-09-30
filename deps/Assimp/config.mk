9f633a0897661e3fa5371d9f2570277713ee2c2f
TheArtist <agorglouk@gmail.com>
2016-10-01 02:53:15 +0300

    Simplified Assimp make configuration

--- deps/Assimp/config.mk
+++ deps/Assimp/config.mk
@@ -1,9 +1,4 @@
 PRJTYPE = StaticLib
-SRC = $(call rwildcard, src, *.cpp) \
-	  $(call rwildcard, src, *.cc) \
-	  $(call rwildcard, src, *.c) \
-	  $(call rwildcard, contrib, *.cpp) \
-	  $(call rwildcard, contrib, *.cc) \
-	  $(call rwildcard, contrib, *.c)
+SRCDIR = src contrib
 DEFINES = ASSIMP_BUILD_NO_OWN_ZLIB ASSIMP_BUILD_BOOST_WORKAROUND ASSIMP_BUILD_NO_C4D_IMPORTER ASSIMP_BUILD_NO_OPENGEX_IMPORTER ASSIMP_BUILD_NO_LWO_IMPORTER ASSIMP_BUILD_NO_LWS_IMPORTER
 ADDINCS = ../AssetLoader/deps/ZLib/include contrib src/BoostWorkaround
